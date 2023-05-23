1aa6fc38aa8b5030321938a6a0af183a6cba251b
DiegoFChC <diegofech15@gmail.com>
2023-05-23 10:32:53 -0500

    bugs login register

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -118,7 +118,7 @@ const Login = () => {
                   type="submit"
                   className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 hover:bg-gray-300 transition-colors"
                   onClick={() => {
-                    login;
+                    login();
                   }}
                 >
                   {t("login")}
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -133,7 +133,7 @@ function Register() {
                   type="submit"
                   className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 hover:bg-gray-300 transition-colors"
                   onClick={() => {
-                    registro;
+                    registro();
                   }}
                 >
                   {t("register")}

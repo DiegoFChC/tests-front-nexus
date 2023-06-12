5cf82c220ec79e75e521c44928cadf5146d9220e
DiegoFChC <diegofech15@gmail.com>
2023-06-12 15:18:42 -0500

    test

--- src/components/alertMessage/AlertMessage.test.js
+++ src/components/alertMessage/AlertMessage.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import AlertMessage from './AlertMessage';
 
@@ -47,5 +47,70 @@ describe('AlertMessage component', () => {
     await waitFor(() => {
       expect(alertMessage).toBeInTheDocument();
     });
+  });
+
+  test('should not close the alert when reason is clickaway', async () => {
+    const message = 'Test message';
+    const type = 'success';
+
+    const { getByText } = render(
+      <AlertMessage message={message} type={type} />
+    );
+
+    const alertMessage = getByText(message);
+    expect(alertMessage).toBeInTheDocument();
+
+    // Simula el clickaway en el Snackbar
+    fireEvent.click(document.body);
+
+    // Espera el tiempo del autoHideDuration
+    jest.advanceTimersByTime(3000);
+
+    // Verifica que el Snackbar aún esté abierto
+    await waitFor(() => {
+      expect(alertMessage).toBeInTheDocument();
+    });
+  });
+
+  // test('should close the alert when reason is not clickaway', async () => {
+  //   const message = 'Test message';
+  //   const type = 'success';
+
+  //   const { getByText, queryByText } = render(
+  //     <AlertMessage message={message} type={type} />
+  //   );
+
+  //   const alertMessage = getByText(message);
+  //   expect(alertMessage).toBeInTheDocument();
+
+  //   fireEvent.click(alertMessage);
+
+  //   await waitFor(() => {
+  //     expect(queryByText(message)).toBeNull();
+  //   });
+  // });
+
+  test('should not close the alert when reason is clickaway', async () => {
+    const message = 'Test message';
+    const type = 'success';
+
+    const { getByText } = render(
+      <AlertMessage message={message} type={type} />
+    );
+
+    const alertMessage = getByText(message);
+    expect(alertMessage).toBeInTheDocument();
+
+    // Simulate close event with 'clickaway' reason
+    fireEvent(alertMessage, new MouseEvent('close', { reason: 'clickaway' }));
+
+    // Verify that the alert message is still visible
+    expect(alertMessage).toBeInTheDocument();
+
+    // Advance timers to handle autoHideDuration
+    jest.advanceTimersByTime(3000);
+
+    // Verify that the alert message does not close
+    expect(alertMessage).toBeInTheDocument();
   });
 });
--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -17,7 +17,6 @@ const Settings = () => {
   return (
     <div className="Settings">
       <div
-        data-testid="click"
         className={`${open ? "menuToggle active" : "menuToggle"}`}
         onClick={() => {
           setOpen(!open);

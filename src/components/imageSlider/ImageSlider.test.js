4d49b244a3677a750fa950d7a4d155d4233e7699
DiegoFChC <diegofech15@gmail.com>
2023-06-12 17:15:43 -0500

    test imageSlider

--- src/components/imageSlider/ImageSlider.test.js
+++ src/components/imageSlider/ImageSlider.test.js
@@ -1,44 +1,111 @@
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // Importa la extensión de expect
 import ImageSlider from "./ImageSlider";
-import {prettyDOM} from "@testing-library/dom"
 
-test("renders content", () => {
-  const componente = render(
-      <ImageSlider />
-  );
-  componente.debug()
+test("renders images correctly", () => {
+  const { getByAltText } = render(<ImageSlider />);
+
+  // Assert that the first image is rendered
+  const firstImage = getByAltText("Image 1");
+  expect(firstImage).toBeInTheDocument();
+
+  // You can also assert the presence of other images if needed
+  // const secondImage = getByAltText("");
+  // const thirdImage = getByAltText("");
+  // ...
+
+  // Add assertions for the rest of the images
 });
 
-test("buscar por img", () => {
-  const componente = render(
-      <ImageSlider />
-  );
-  const img = componente.container.querySelector("img")
-  console.log(prettyDOM(img))
+test("slides to previous image when left arrow is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking the left arrow
+  const leftArrow = getByAltText("arrow1");
+  fireEvent.click(leftArrow);
+
+  // Assert that the previous image is displayed
+  const previousImage = getByAltText("Image 5");
+  expect(previousImage).toBeInTheDocument();
 });
 
-test("buscar por button", () => {
-  const componente = render(
-      <ImageSlider />
-  );
-  const button = componente.container.querySelector("button")
-  console.log(prettyDOM(button))
+test("slides to next image when right arrow is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking the right arrow
+  const rightArrow = getByAltText("arrow2");
+  fireEvent.click(rightArrow);
+
+  // Assert that the next image is displayed
+  const nextImage = getByAltText("Image 2");
+  expect(nextImage).toBeInTheDocument();
 });
 
-test("buscar por setImage", () => {
-  const componente = render(
-      <ImageSlider />
-  );
-  const setImage = componente.container.querySelector("setImage")
-  console.log(prettyDOM(setImage))
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-2");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 2");
+  expect(thirdImage).toBeInTheDocument();
 });
 
-test("buscar por if", () => {
-  const componente = render(
-      <ImageSlider />
-  );
-  const ifa = componente.container.querySelector("if")
-  console.log(prettyDOM(ifa))
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-1");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 1");
+  expect(thirdImage).toBeInTheDocument();
+});
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-2");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 2");
+  expect(thirdImage).toBeInTheDocument();
+});
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-3");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 3");
+  expect(thirdImage).toBeInTheDocument();
+});
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-4");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 4");
+  expect(thirdImage).toBeInTheDocument();
+});
+test("slides to a specific image when button is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Simulate clicking a specific button (e.g., button for the third image)
+  const thirdButton = getByTestId("btn-5");
+  fireEvent.click(thirdButton);
+
+  // Assert that the corresponding image is displayed
+  const thirdImage = getByAltText("Image 5");
+  expect(thirdImage).toBeInTheDocument();
 });

09640d61a19cda2ad1243ee2c431da5dd7c25d6a
DiegoFChC <diegofech15@gmail.com>
2023-06-12 12:49:58 -0500

    tests

---
+++ src/components/cardRoom/CardRoom.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import CardRoom from "./CardRoom";
+import { BrowserRouter } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+test("renders content cardroom", () => {
+  const componente = render(
+    <BrowserRouter>
+      <I18nextProvider i18n={i18next}>
+        <CardRoom />
+      </I18nextProvider>
+    </BrowserRouter>
+  );
+  componente.debug();
+});

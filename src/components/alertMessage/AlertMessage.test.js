5ab57d07d1f8ef59234c5967434187022c8e2ecc
DiegoFChC <diegofech15@gmail.com>
2023-06-12 14:34:21 -0500

    CardMenu AlertMessage tests

--- src/components/alertMessage/AlertMessage.test.js
+++ src/components/alertMessage/AlertMessage.test.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, fireEvent } from "@testing-library/react";
-import AlertMessage from "./AlertMessage";
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import AlertMessage from './AlertMessage';
 
-test("renders content alert", () => {
-  const componente = render(
-      <AlertMessage />
-  );
-  componente.debug();
+jest.useFakeTimers(); // Simula el temporizador para el autoHideDuration
+
+describe('AlertMessage component', () => {
+  test('should render message and close the alert after autoHideDuration', async () => {
+    const message = 'Test message';
+    const type = 'success';
+
+    const { getByText, queryByText } = render(
+      <AlertMessage message={message} type={type} />
+    );
+
+    const alertMessage = getByText(message);
+    expect(alertMessage).toBeInTheDocument();
+
+    // Espera el tiempo del autoHideDuration
+    jest.advanceTimersByTime(3000);
+
+    // Espera a que el Snackbar se cierre
+    await waitFor(() => {
+      expect(queryByText(message)).toBeNull();
+    });
+  });
+
+  test('should not close the alert when clickaway', async () => {
+    const message = 'Test message';
+    const type = 'success';
+
+    const { getByText } = render(
+      <AlertMessage message={message} type={type} />
+    );
+
+    const alertMessage = getByText(message);
+    expect(alertMessage).toBeInTheDocument();
+
+    // Simula el click en el Snackbar
+    fireEvent.click(alertMessage);
+
+    // Espera el tiempo del autoHideDuration
+    jest.advanceTimersByTime(3000);
+
+    // Verifica que el Snackbar aún esté abierto
+    await waitFor(() => {
+      expect(alertMessage).toBeInTheDocument();
+    });
+  });
 });
-
--- src/components/buttonIcon/ButtonIcon.test.js
+++ src/components/buttonIcon/ButtonIcon.test.js
@@ -1,45 +1,27 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { BrowserRouter } from "react-router-dom";
-
 import { render, fireEvent } from "@testing-library/react";
 import ButtonIcon from "./ButtonIcon";
-import { changeLanguage } from "../../functions/changeLanguage";
+import { BrowserRouter } from "react-router-dom";
 
-// Mockear la función changeLanguage
-jest.mock("../../functions/changeLanguage", () => ({
-  changeLanguage: jest.fn(),
-}));
+test("renders content", () => {
+  const componente = render(
+    <BrowserRouter>
+      <ButtonIcon />
+    </BrowserRouter>
+  );
+  componente.debug()
+});
 
-// Mockear la función navigate
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => ({
-  useNavigate: () => mockNavigate,
-}));
+test("Button click", () => {
 
-describe("ButtonIcon component", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  const component = render(
+    <BrowserRouter>
+      <ButtonIcon />
+    </BrowserRouter>
+  );
 
-  test('render component ButtonIcon"', () => {
-    const component = render(
-      <ButtonIcon text="Button" icon="icon.png" link="language" />
-    );
-    component.debug()
-  });
+  const button = component.getByTestId("click");
+  fireEvent.click(button);
 
-  test('should call changeLanguage when link is "language"', () => {
-    const { getByTestId } = render(
-      <ButtonIcon text="Button" icon="icon.png" link="language" />
-    );
-    const button = getByTestId("click");
-
-    fireEvent.click(button);
-
-    expect(mockNavigate).not.toHaveBeenCalled();
-    expect(changeLanguage).toHaveBeenCalledTimes(1);
-    expect(changeLanguage).toHaveBeenCalledWith(expect.any(Object));
-  });
-  
 });

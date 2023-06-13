e83bdec334f5d3a2b1b3ef8e1189385b38448d36
DiegoFChC <diegofech15@gmail.com>
2023-06-13 06:47:46 -0500

    test CardRoom

--- src/components/imageSlider/ImageSlider.test.js
+++ src/components/imageSlider/ImageSlider.test.js
@@ -30,6 +30,55 @@ test("slides to previous image when left arrow is clicked", () => {
   expect(previousImage).toBeInTheDocument();
 });
 
+test("slides to the previous image when left arrow is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Set the initial image to be the first one
+  const initialImage = getByAltText("Image 1");
+  expect(initialImage).toHaveAttribute("src", "img1.png");
+
+  // Simulate clicking the left arrow when image is 0
+  const leftArrow = getByAltText("arrow1");
+  fireEvent.click(leftArrow);
+
+  // Assert that the image state is updated correctly to the last image (img5.png)
+  const updatedImage = getByAltText("Image 5");
+  expect(updatedImage).toHaveAttribute("src", "img5.png");
+});
+
+test("slides to the next image when right arrow is clicked", () => {
+  const { getByAltText, getByTestId } = render(<ImageSlider />);
+
+  // Set the initial image to be the first one
+  const initialImage = getByAltText("Image 1");
+  expect(initialImage).toHaveAttribute("src", "img1.png");
+
+  // Simulate clicking the right arrow when image is 0
+  const rightArrow = getByAltText("arrow2");
+  fireEvent.click(rightArrow);
+
+  // Assert that the image state is updated correctly to the second image (img2.png)
+  const updatedImage = getByAltText("Image 2");
+  expect(updatedImage).toHaveAttribute("src", "img2.png");
+
+  fireEvent.click(rightArrow);
+  const updatedImage3 = getByAltText("Image 3");
+  expect(updatedImage3).toHaveAttribute("src", "img3.png");
+  
+  fireEvent.click(rightArrow);
+  const updatedImage4 = getByAltText("Image 4");
+  expect(updatedImage4).toHaveAttribute("src", "img4.png");
+
+  fireEvent.click(rightArrow);
+  const updatedImage5 = getByAltText("Image 5");
+  expect(updatedImage5).toHaveAttribute("src", "img5.png");
+  
+  fireEvent.click(rightArrow);
+  const updatedImage1 = getByAltText("Image 1");
+  expect(updatedImage1).toHaveAttribute("src", "img1.png");
+  
+});
+
 test("slides to next image when right arrow is clicked", () => {
   const { getByAltText, getByTestId } = render(<ImageSlider />);
 
